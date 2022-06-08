8a98edc57bb4febb586a8025e7227fab7c7c30e8
Gauss3Maxwell <Gauss3Maxwell@outlook.com>
2022-06-08 14:19:48 +0800

    use as List<dynamic>

--- lib/modules/home/domain/services/home_api.dart
+++ lib/modules/home/domain/services/home_api.dart
@@ -4,15 +4,7 @@ class HomeApi {
   Future<List<dynamic>> getBanners() async {
     final dio = Dio();
     final response = await dio.get('${AppConstants.randomApiUrl}/banners');
-    debugPrint("========================> test ");
-    debugPrint(response.toString());
-    debugPrint("========================> test");
-    final data = response.data;
-    return List<Map<String, dynamic>>.from(
-      data.map(
-        (e) => Map<String, dynamic>.from(e as Map<String, dynamic>),
-      ),
-    );
+    return response.data as List<dynamic>;
   }
 
   //get Quotations
