7012df44b808d003602f44796c54461745d19f0d
Gauss3Maxwell <Gauss3Maxwell@outlook.com>
2022-07-29 11:21:13 +0800

    Release multi version app

---
+++ flutter.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+modifyflutter(){
+title1='<application android:requestLegacyExternalStorage="true" android:name="io.flutter.app.FlutterApplication" android:icon="@mipmap/ic_launcher" android:label="'
+title2='" android:networkSecurityConfig="@xml/network_security_config" android:usesCleartextTraffic="true">'
+#echo $title1
+#echo $title2
+title=$title1$1$title2
+#echo $title
+echo start create $1, please wait...
+#title   important line need modify
+sed -i "11c${title}" ./android/app/src/main/AndroidManifest.xml
+#image
+cp -r ./resources/image/$1/icon/* ./android/app/src/main/res/
+cp -r ./resources/image/$1/image/* ./assets/images/
+#cp -r /home/rui/wang/image/$1/image/* /home/rui/mnt/mars/assets/images/2.0x/
+#cp -r /home/rui/wang/image/$1/image/* /home/rui/mnt/mars/assets/images/3.0x/
+#version
+
+version='applicationId "com.'$1'.mnt"'
+#echo $version
+sed -i "41c${version}" ./android/app/build.gradle
+#walletpath
+walletpath="name: '"$1"',"
+#echo $walletpath
+sed -i "6c${walletpath}" ./lib/modules/wallet/domain/utils/wallet_path_utils.dart
+#dpos
+dpos='\"'$2'\";'
+#echo dpos
+sed -i "63c${dpos}" ./lib/modules/app/config/app_constants.dart
+cd ./
+dposname=${2:47:10}
+# echo $dposname
+#if ["$3" = '1']; then
+flutter build apk --no-sound-null-safety
+cp -r ./build/app/outputs/flutter-apk/app-release.apk ./RenameApp/$1.$dposname.apk
+#fi
+}
+# modifyflutter Durian 20m07jr2smarkbab2bwatddzt56wd9sms0q2gkg2k3yhgnxby9mzrzbvc
+# modifyflutter Chestnut 20m0ek0kz2pdvvrmennmpp02pdpwbr1kfj8ayj2119v9vhnkdee9q2bga
+# modifyflutter Watermelon  20m0dygcs3mwznnqd4q2gfcp98x9905ape5sf3fj4cf4w4fy6xcspf2qg
+# modifyflutter Noni 20m02mzahhqfbj3zwke22w75r9vagwwmarr6e326mcgcz9zzk4z6m2e5p
+# modifyflutter Apple 20m06vp0mw0d4s6vvkc22nxv8kr7345156z07csnjj39x0pjc929mqc9c
+# modifyflutter Passionfruit 20m07m5nyy63ngawb76qhnts1xyfk9c3yc56sjv711vadnxq05m1gxy1t
+# modifyflutter Lemon  20m055bb12ymfr8v507vwxzyw82vf8fpnr8bx170axdp92cgrhwakytzm
+modifyflutter Orange  20m09jjm09xf63f5g00wv4r33ajqbzetr0mffb4mqem581f6vck39eqn4
--- lib/modules/app/config/app_constants.dart
+++ lib/modules/app/config/app_constants.dart
@@ -60,7 +60,7 @@ class AppConstants {
 
   static const fiatPrecision = 2;
   static const dposAddress =
-      '20m09jjm09xf63f5g00wv4r33ajqbzetr0mffb4mqem581f6vck39eqn4';
+"20m09jjm09xf63f5g00wv4r33ajqbzetr0mffb4mqem581f6vck39eqn4";
 
   static String get sentryDns => isBeta ? sentryDnsBeta : sentryDnsProd;
 
