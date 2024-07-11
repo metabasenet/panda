30a3f4523bd1a6a042a9b0c235574d4e83c90bd1
lonicera <lonicera.xyz@gmail.com>
2024-07-11 09:58:18 +0800

    Update i18n, fix swap bugs.

--- packages/kit/src/components/TokenListItem/index.tsx
+++ packages/kit/src/components/TokenListItem/index.tsx
@@ -1,11 +1,7 @@
-import {
-  NumberSizeableText,
-  SizableText,
-  Stack,
-  XStack,
-} from '@onekeyhq/components';
+import { NumberSizeableText, SizableText, Stack } from '@onekeyhq/components';
 import type { IListItemProps } from '@onekeyhq/kit/src/components/ListItem';
 import { ListItem } from '@onekeyhq/kit/src/components/ListItem';
+import type { IFuseResultMatch } from '@onekeyhq/shared/src/modules3rdParty/fuse';
 import accountUtils from '@onekeyhq/shared/src/utils/accountUtils';
 
 import { Token } from '../Token';
@@ -20,6 +16,7 @@ export type ITokenListItemProps = {
   isSearch?: boolean;
   valueProps?: { value: string; currency?: string };
   disabled?: boolean;
+  titleMatchStr?: IFuseResultMatch;
 } & IListItemProps;
 
 export function TokenListItem({
@@ -32,6 +29,7 @@ export function TokenListItem({
   balance,
   valueProps,
   disabled,
+  titleMatchStr,
   ...rest
 }: ITokenListItemProps) {
   return (
@@ -40,67 +38,70 @@ export function TokenListItem({
       {...(disabled && {
         opacity: 0.5,
       })}
+      {...(isSearch && {
+        $md: {
+          alignItems: 'flex-start',
+        },
+      })}
+      {...rest}
     >
-      <XStack flex={1} justifyContent="space-between">
-        <XStack
-          space="$3"
-          alignItems="flex-start"
-          $gtMd={{ alignItems: 'center' }}
-        >
-          <Token
-            tokenImageUri={tokenImageSrc}
-            networkImageUri={networkImageSrc}
-          />
-          <ListItem.Text
-            primary={tokenSymbol}
-            primaryMatch={rest.titleMatch}
-            secondary={
-              isSearch ? (
-                <Stack space="$1" $gtMd={{ flexDirection: 'row' }}>
-                  <SizableText color="$textSubdued" size="$bodyMd">
-                    {tokenName}
-                  </SizableText>
-                  <SizableText color="$textDisabled" size="$bodyMd">
-                    {accountUtils.shortenAddress({
-                      address: tokenContrastAddress,
-                      leadingLength: 8,
-                      trailingLength: 6,
-                    })}
-                  </SizableText>
-                </Stack>
-              ) : (
-                tokenName ?? ''
-              )
-            }
-          />
-        </XStack>
-        <ListItem.Text
-          align="right"
-          primary={
+      <Token tokenImageUri={tokenImageSrc} networkImageUri={networkImageSrc} />
+      <ListItem.Text
+        flex={1}
+        primary={tokenSymbol}
+        primaryMatch={titleMatchStr}
+        primaryTextProps={{
+          numberOfLines: 1,
+        }}
+        secondary={
+          isSearch ? (
+            <Stack space="$0.5" $gtMd={{ flexDirection: 'row', space: '$1' }}>
+              <SizableText
+                numberOfLines={1}
+                color="$textSubdued"
+                size="$bodyMd"
+              >
+                {tokenName}
+              </SizableText>
+              <SizableText color="$textDisabled" size="$bodyMd">
+                {accountUtils.shortenAddress({
+                  address: tokenContrastAddress,
+                  leadingLength: 8,
+                  trailingLength: 6,
+                })}
+              </SizableText>
+            </Stack>
+          ) : (
+            tokenName ?? ''
+          )
+        }
+      />
+      <ListItem.Text
+        align="right"
+        primary={
+          <NumberSizeableText
+            textAlign="right"
+            color="$text"
+            formatter="balance"
+            size="$bodyLgMedium"
+          >
+            {balance}
+          </NumberSizeableText>
+        }
+        secondary={
+          valueProps?.value ? (
             <NumberSizeableText
               textAlign="right"
-              color="$text"
-              formatter="balance"
-              size="$bodyLgMedium"
+              size="$bodyMd"
+              formatter="value"
+              color="$textSubdued"
+              formatterOptions={{ currency: valueProps?.currency ?? '$' }}
             >
-              {balance}
+              {valueProps.value}
             </NumberSizeableText>
-          }
-          secondary={
-            valueProps?.value ? (
-              <NumberSizeableText
-                textAlign="right"
-                size="$bodyMd"
-                formatter="value"
-                color="$textSubdued"
-                formatterOptions={{ currency: valueProps?.currency ?? '$' }}
-              >
-                {valueProps.value}
-              </NumberSizeableText>
-            ) : null
-          }
-        />
-      </XStack>
+          ) : null
+        }
+      />
     </ListItem>
   );
 }
